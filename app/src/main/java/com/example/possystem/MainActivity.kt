9ff516aa4af80c426ed65e91bd41ea0a4cb1c8e3
ubayasiri <ugevindu@yahoo.com>
2021-07-01 17:56:33 +0530

    Login

---
+++ app/src/main/java/com/example/possystem/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.possystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+
+        setContentView(R.layout.activity_main)
+    }
+}
